9a6219704069aa1243930cda938f763944e8a85c
Shailesh <shaileshsiingh@gmail.com>
2023-06-11 16:09:26 +0530

    React Exp

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <div><h2>Expense Items</h2>
+   <h3> Food</h3><p>Rs 100</p>
+    
+    <h3> Petrol</h3>
+    <p>Rs 200</p>
+    <h3> Movies</h3>
+    <p>Rs 300</p>
+    </div>
+    
+  )
+}
+
+export default ExpenseItem
