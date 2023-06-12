1db44e9ed47f89841c4e77e68f4b9ae2efa74a77
Shailesh Singh <shaileshsiingh@gmail.com>
2023-06-12 15:30:39 +0530

    cstyling css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import './ExpenseItem.css'
 
 function ExpenseItem() {
+const expenseDate = new Date(2021,2,28)
+const expenseTitle = 'Car Insurance'
+const expenseAmount = 298
+const locationOfExpenditure = 'Mumbai'
+
   return (
-    <div><h2>Expense Items</h2>
-   <h3> Food</h3><p>Rs 100</p>
-    
-    <h3> Petrol</h3>
-    <p>Rs 200</p>
-    <h3> Movies</h3>
-    <p>Rs 300</p>
+    <div>
+    <div className='expense-item'>
+      {expenseDate.toDateString()}
+    </div>
+    <div className='expense-item__description'>
+      <h2>{expenseTitle}</h2>
+      <div className='expense-item__price'>{expenseAmount}</div>
+      <div className='expense-item__price'>{locationOfExpenditure}</div>
+    </div>
     </div>
     
   )
