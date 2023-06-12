64da1eccfdfa10f96f64cd29b12a4905a68f4034
Shailesh Singh <shaileshsiingh@gmail.com>
2023-06-12 17:07:18 +0530

    Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-const expenseDate = new Date(2021,2,28)
-const expenseTitle = 'Car Insurance'
-const expenseAmount = 298
-const locationOfExpenditure = 'Mumbai'
+function ExpenseItem(props) {
+// const locationOfExpenditure = 'Mumbai'
 
   return (
     <div>
     <div className='expense-item'>
-      {expenseDate.toDateString()}
+      {props.date.toDateString()}
     </div>
     <div className='expense-item__description'>
-      <h2>{expenseTitle}</h2>
-      <div className='expense-item__price'>{expenseAmount}</div>
-      <div className='expense-item__price'>{locationOfExpenditure}</div>
+      <h2>{props.title}</h2>
+      <div className='expense-item__price'>{props.amount}</div>
+      <div className='expense-item__price'>{props.location}</div>
     </div>
     </div>
     
