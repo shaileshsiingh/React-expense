3ffb8538c1b3b427a366606ffee414f4e5b2ed8b
Shailesh Singh <shaileshsiingh@gmail.com>
2023-06-15 01:00:30 +0530

    Arrow functions and file restructure

---
+++ src/components/Expenses/ExpenseDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ExpenseDetails(props) {
+  return (
+    <>
+    <div className='expense-item__description'>
+    <h2>{props.title}</h2>
+      <div className='expense-item__price'>{props.amount}</div>
+      <div className='expense-item__price'>{props.location}</div>
+      </div>
+      </>
+  )
+}
+
+export default ExpenseDetails
+
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
+import Card from '..UI/Card'
+
+function Expenses(props) {
+  return (
+    <Card classsname='expenses'> 
+        {props.expenses && props.expenses.map((expense) => (
+      <ExpenseItem
+      title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          location={expense.location}
+          
+      />
+      ))}
+    </Card>
+  )
+}
+
+export default Expenses
