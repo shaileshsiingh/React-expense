50251ee2cef4ca021f98c1ccdcfd005a758eece7
Shailesh Singh <shaileshsiingh@gmail.com>
2023-06-15 00:41:51 +0530

    Composing components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import ExpenseDetails from './ExpenseDetails'
+import Card from './Card'
 
 function ExpenseItem(props) {
 // const locationOfExpenditure = 'Mumbai
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date = {props.date} />
     <div className='expense-item__description'>
     <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
     </div>
-    </div>
+    </Card>
     
   )
 }
