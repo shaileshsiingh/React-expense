3434826dd26f318b0cd0cc1af5f02a48ca8ecd86
Shailesh Singh <shaileshsiingh@gmail.com>
2023-06-17 14:04:43 +0530

    Delete an expense with Dom manipulation

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,17 @@ import ExpenseDetails from './ExpenseDetails'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
-// const locationOfExpenditure = 'Mumbai
+const deleteHandler = () =>{
+ const expenseItem = document.querySelector('.expense-item');
+    expenseItem.remove();
+}
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date = {props.date} />
     <div className='expense-item__description'>
-    <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+    <ExpenseDetails amount= {props.amount}  location={props.location}   title={props.title} />
+    <button className='delete' onClick={deleteHandler}>Delete</button>
     </div>
     </Card>
     
