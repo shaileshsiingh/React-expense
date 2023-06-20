9b6fc3e213cbf4ca0cb93643647942a8d59da7f1
Shailesh Singh <shaileshsiingh@gmail.com>
2023-06-20 18:00:47 +0530

    added Filtered UI

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
 
-import React from 'react';
+import React, {useState} from 'react';
+import ExpensesFilter from './ExpensesFilter';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
   return (
     <Card className="expenses">
+              <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+
       {props.items.map((item)=>(
         <ExpenseItem
         title={item.title}
